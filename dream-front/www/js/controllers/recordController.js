25bd12419bda879456fe39e13b7dfd4d17ac4bf0
Mendiondo Loris <loris.mendiondo@gmail.com>
2014-10-10 16:01:18 +0200

    Record page
    This is just the base, not working yet

--- dream-front/www/js/controllers/recordController.js
+++ dream-front/www/js/controllers/recordController.js
@@ -1,5 +1,38 @@
-ctrl.controller('RecordController', function ($scope) {
+ctrl.controller('RecordController', function ($scope, $ionicPopup) {
   $scope.navTitle = "Record";
+
+
+//popup function for text and microphone recording
+$scope.showPopup = function(templateUrl, title, errorMessage) {
+  $scope.data = {}
+  $scope.errorMessage = ""
+
+  var myPopup = $ionicPopup.show({
+    templateUrl: templateUrl,
+    title: title,
+    scope: $scope,
+    buttons: [
+    { text: 'Cancel' },
+    {
+      text: '<b>Save</b>',
+      type: 'button-positive',
+      onTap: function(e) {
+        if (!$scope.data.dream) {
+          $scope.errorMessage = errorMessage
+          e.preventDefault();
+        } else {
+          //process datas
+          return $scope.data;
+        }
+      }
+    },
+    ]
+  });
+  myPopup.then(function(res) {
+    myPopup.close()
+    console.log('Dream recorded', res);
+  });
+};
 
   $scope.leftButtons = [{
     type: 'button-icon icon ion-navicon',
