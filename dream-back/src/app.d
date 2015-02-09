7c5030929b530da353a1068f30335a3f98db9a48
Julien Ganichot <julien.ganichot@gmail.com>
2015-02-09 17:49:38 +0100

    Added OPTIONS handling

--- dream-back/src/app.d
+++ dream-back/src/app.d
@@ -69,6 +69,7 @@ shared static	this()
       }
 			myApi = new DreamAPI(connStr);
       router.any("*", &setAccessControlOrigin);
+			router.match(HTTPMethod.OPTIONS, "*", &setAccessControlOrigin);
       try registerRestInterface(router, myApi);
       catch(Exception e) {
 				writeln("Failed to register interface: ", e.toString());
