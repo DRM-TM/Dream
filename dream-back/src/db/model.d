db1393fc04c64dcd53c2b0f54793b5563faf26c5
Julien Ganichot <julien.ganichot@gmail.com>
2015-02-10 09:54:33 +0100

    Added more exceptions

--- dream-back/src/db/model.d
+++ dream-back/src/db/model.d
@@ -160,7 +160,7 @@ class Model(T, U) {
       try result = c.execSQLSequence();
       catch (Exception e) {
         result.close();
-        return (false);
+        throw new HTTPStatusException(204);
       }
       result.close();
       return (true);
@@ -180,7 +180,7 @@ class Model(T, U) {
         try result = c.execSQLSequence();
         catch (Exception e) {
           result.close();
-          return (false);
+          throw new HTTPStatusException(204);
         }
         result.close();
         return (true);
